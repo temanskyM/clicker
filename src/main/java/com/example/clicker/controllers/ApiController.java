de2c51906b84f48c6352cb38918121c7a4ef765a
temansky <temansky@yandex.ru>
2021-03-01 20:32:04 +0300

    Добавлен Api для получения значения кликера

--- src/main/java/com/example/clicker/controllers/ApiController.java
+++ src/main/java/com/example/clicker/controllers/ApiController.java
@@ -4,22 +4,31 @@ import com.example.clicker.models.Clicker;
 import com.example.clicker.services.ClickerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class ApiController {
-    private final AtomicInteger counter = new AtomicInteger();
+    private AtomicInteger counter = new AtomicInteger();
 
     private final ClickerService clickerService;
 
     public ApiController(ClickerService clickerService) {
         this.clickerService = clickerService;
+
+        //Получаем текущее значение счетчика
+        counter.set(clickerService.get().getCount());
+    }
+
+    @GetMapping("/api/clicker")
+    public Clicker getClicker() {
+        return clickerService.get();
     }
 
     @GetMapping("/api/counter")
-    public Clicker getClicker() {
-        return clickerService.get();
+    public Integer getCounter() {
+        return counter.get();
     }
 }
