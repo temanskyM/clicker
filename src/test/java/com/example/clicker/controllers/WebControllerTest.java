b0549ade4edf8f1e328b77f07321f11329812ab1
temansky <temansky@yandex.ru>
2021-03-01 22:53:42 +0300

    Добавлены тесты WebController

--- src/test/java/com/example/clicker/controllers/WebControllerTest.java
+++ src/test/java/com/example/clicker/controllers/WebControllerTest.java
@@ -1,20 +1,23 @@
 package com.example.clicker.controllers;
 
+import com.example.clicker.models.Clicker;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class WebControllerTest {
-
-//    @Autowired
-//    WebController webController;
+    @Autowired
+    private TestRestTemplate restTemplate;
 
     @Test
-    void home() {
-
-
+    void whenGetHome_ReturnStatus200() {
+        ResponseEntity<Clicker> response = restTemplate.getForEntity("/", null);
+        assertEquals(response.getStatusCode(), HttpStatus.OK);
     }
 }
