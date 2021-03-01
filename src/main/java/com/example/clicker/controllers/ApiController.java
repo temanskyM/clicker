670690f315b1cd4fe77b4b4b3538f5a1c47dbe67
temansky <temansky@yandex.ru>
2021-03-01 21:17:20 +0300

    Добавлены функции получения и инкрементации счетчика на главной странице, а так же добавлена обработка кнопки buttonIncrementCounter на основной странице

--- src/main/java/com/example/clicker/controllers/ApiController.java
+++ src/main/java/com/example/clicker/controllers/ApiController.java
@@ -2,14 +2,9 @@ package com.example.clicker.controllers;
 
 import com.example.clicker.models.Clicker;
 import com.example.clicker.services.ClickerService;
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import javax.annotation.PreDestroy;
-import java.util.concurrent.atomic.AtomicInteger;
 
 @RestController
 public class ApiController {
